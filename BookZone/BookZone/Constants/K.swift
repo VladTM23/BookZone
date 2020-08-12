b457baa266b0ea29bfeb4b28cf40be4a8584e860
VladTM23 <vlad.paianu@gmail.com>
2020-08-12 18:47:14 +0300

    Added API inside ViewController.

---
+++ BookZone/BookZone/Constants/K.swift
@@ -0,0 +1,19 @@
+//
+//  K.swift
+//  BookZone
+//
+//  Created by Paianu Vlad-Valentin on 12/08/2020.
+//  Copyright © 2020 Paianu Vlad-Valentin. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let key = "RNsVx8TWoCWvPVwuzzV4A"
+    static let secret = "rBBSJQtj0PjyIZ4uTi33tESjTvRZ6EKYFkCnbBGiyg"
+
+    struct Endpoints {
+        static let isbnURL = "https://www.goodreads.com/book/isbn/"
+        static let titleURL = "https://www.goodreads.com/book/title.xml?"
+    }
+}
